eac3c9b32851ebf7e5d096d31f46081391f22cc6
DiMs8P <dota.dimabog@mail.ru>
2024-09-19 16:27:38 +0700

    refactoring

---
+++ RGINPO_WinForms/CustomChart.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Windows.Forms;
+
+namespace RGINPO_WinForms
+{
+    public class CustomChart
+    {
+        public ChartArea chartArea1 = new();
+        public Legend legend1 = new();
+        public Series series1 = new();
+        public Chart chart1 = new();
+
+        public void InitChart()
+        {
+            chartArea1.Name = "ChartArea1";
+            chart1.ChartAreas.Add(chartArea1);
+            legend1.Enabled = false;
+            legend1.Name = "Legend1";
+            chart1.Legends.Add(legend1);
+            chart1.Location = new Point(12, 16);
+            chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series1.XValueMember = "X";
+            series1.YValueMembers = "Y";
+            chart1.Series.Add(series1);
+            chart1.Size = new Size(300, 300);
+            chart1.TabIndex = 2;
+            chart1.Text = "chart1";
+        }
+    }
+}
--- RGINPO_WinForms/Form1.cs
+++ RGINPO_WinForms/Form1.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Windows.Forms.DataVisualization.Charting;
 
 namespace RGINPO_WinForms
@@ -5,14 +6,19 @@ namespace RGINPO_WinForms
     public partial class Form1 : Form
     {
         public BindingSource Data { get; set; }
+        private CustomChart CustomChart { get; set; }
+        
         public Form1()
         {
             Data = new BindingSource();
+            CustomChart = new CustomChart();
+
             Data.Add(new Data() { X = 0, Y = 0 });
             Data.Add(new Data() { X = 1, Y = 1 });
             Data.Add(new Data() { X = 2, Y = 4 });
             InitializeComponent();
-            InitKALL();
+            CustomChart.InitChart();
+            Controls.Add(CustomChart.chart1);
 
             comboBox1.Items.Add("Draw as line");
             comboBox1.Items.Add("Draw as spine");
@@ -24,45 +30,17 @@ namespace RGINPO_WinForms
             Data.Add(new Data() { X = 4, Y = 16 });
         }
 
-        private void DrawAsLines_Click()
+        private void DrawAsLines()
         {
-            chart1.DataSource = null;
-            chart1.Series[0].ChartType = SeriesChartType.Line;
-            chart1.DataSource = Data;
+            CustomChart.chart1.DataSource = null;
+            CustomChart.chart1.Series[0].ChartType = SeriesChartType.Line;
+            CustomChart.chart1.DataSource = Data;
         }
-        private void DrawAsSpline_Click()
+        private void DrawAsSpline()
         {
-            chart1.DataSource = null;
-            chart1.Series[0].ChartType = SeriesChartType.Spline;
-            chart1.DataSource = Data;
-        }
-
-
-        private ChartArea chartArea1 = new();
-        private Legend legend1 = new();
-        private Series series1 = new();
-        private Chart chart1 = new();
-
-        private void InitKALL()
-        {
-            chartArea1.Name = "ChartArea1";
-            chart1.ChartAreas.Add(chartArea1);
-            legend1.Enabled = false;
-            legend1.Name = "Legend1";
-            chart1.Legends.Add(legend1);
-            chart1.Location = new Point(12, 16);
-            chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            series1.XValueMember = "X";
-            series1.YValueMembers = "Y";
-            chart1.Series.Add(series1);
-            chart1.Size = new Size(300, 300);
-            chart1.TabIndex = 2;
-            chart1.Text = "chart1";
-
-            this.Controls.Add(chart1);
+            CustomChart.chart1.DataSource = null;
+            CustomChart.chart1.Series[0].ChartType = SeriesChartType.Spline;
+            CustomChart.chart1.DataSource = Data;
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -70,10 +48,10 @@ namespace RGINPO_WinForms
             switch (comboBox1.SelectedIndex)
             {
                 case 0:
-                    DrawAsLines_Click();
+                    DrawAsLines();
                     break;
                 case 1:
-                    DrawAsSpline_Click();
+                    DrawAsSpline();
                     break;
             }
         }
